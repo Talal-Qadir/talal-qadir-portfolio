d530d96307914b720998cf227410d4fcc2bb66b6
Talal-Qadir <qadirmuhammadtalal@gmail.com>
2026-06-19 11:23:54 +0500

    remove db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,6 +78,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
